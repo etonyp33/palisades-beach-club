446fc2fdec57f23ca1bb3f115606997d85f598bd
etonyp33 <etonyp@gmail.com>
2025-04-15 17:32:32 -0700

    roster fix

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -33,7 +33,7 @@ const Roster = () => {
             <hr></hr>
             <iframe
               id="rosterIframe"
-              src="files/roster.pdf"
+              src="files/roster.docx"
               width={"1200px"}
               height={"800px"}
             ></iframe>
